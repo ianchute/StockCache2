55d47ebddd771d1b9ee687c429f241d46b2e5078
Ian Herve Chu Te <ianhervechute@gmail.com>
2017-01-05 17:22:27 +0800

    Sample random viz.

--- src/cljs/stockcache2/core.cljs
+++ src/cljs/stockcache2/core.cljs
@@ -4,13 +4,22 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
+(def app-state (atom {:data (range 0 256)}))
 
 (defn root-component [app owner]
   (reify
     om/IRender
     (render [_]
-      (dom/div nil (dom/h1 nil (:text app))))))
+      (dom/div #js { :style #js { :pointerEvents "none" :top "calc(50% - 250px)" :left "calc(50% - 150px)" :position "absolute" } }
+        (reverse
+          (map #(dom/div #js { :className "fade" :key (str "particle_" %) :style #js {
+
+            :color (str "rgba(" (rand-int 256) "," (rand-int 256) "," (rand-int 256) "," 0.8 ")")
+            :fontSize (str (/ % 1.2) "px")
+            :textAlign "center"
+            :position "absolute"
+            :top (str (rand-int 300) "px")
+            :left (str (rand-int 300) "px") } } "•") (:data app)))))))
 
 (om/root
  root-component
